65a05f3689263f28cfbca84710929d8f358f4091
Daniel Lim-Apo <daniel.github@sede.com.br>
2024-08-08 08:50:35 -0300

    Update to fix example 003 comment

--- 003-ArraySum/arraySum.js
+++ 003-ArraySum/arraySum.js
@@ -1,8 +1,8 @@
 function sumArray(arr) {
-    if (arr.length === 0) {
-        return 0; // Base case
-    }
-    return arr[0] + sumArray(arr.slice(1)); // Recursive case
+  if (arr.length === 0) {
+    return 0; // Base case
+  }
+  return arr[0] + sumArray(arr.slice(1)); // Recursive case
 }
 
-console.log(sumArray([1, 2, 3, 4, 5, 100])); // Expected Output: 1s15
+console.log(sumArray([1, 2, 3, 4, 5, 100])); // Expected Output: 115
